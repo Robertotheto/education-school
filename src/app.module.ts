f5bdf12d279670c20e538a633dd687e5a1a01e82
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2021-12-12 20:36:23 -0400

    Prisma(module, service)
    Create file of service and module prisma client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { TeachersModule } from './teachers/teachers.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, TeachersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
