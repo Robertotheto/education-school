c310935f38338ea2082adf01c471aa4623c777a2
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2021-12-12 21:30:31 -0400

    Validator
    Create validation dto teacher.

--- src/teachers/dto/create-teacher.dto.ts
+++ src/teachers/dto/create-teacher.dto.ts
@@ -1,8 +1,20 @@
+import { IsEmail, IsEmpty, IsNumber, IsString } from 'class-validator';
 import { Teacher } from '../entities/teacher.entity';
 
 export class CreateTeacherDto extends Teacher {
+  @IsString()
+  @IsEmpty()
   name: string;
+
+  @IsEmail()
+  @IsEmpty()
   email: string;
+
+  @IsNumber()
+  @IsEmpty()
   cpf: number | bigint;
+
+  @IsString()
+  @IsEmpty()
   formation: string;
 }
