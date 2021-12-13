0bfa702743cf7fe887827abc0df735298d95f450
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2021-12-12 20:53:29 -0400

    Teachers(dto, entity)
    create dto and entity in Teachers

--- src/teachers/dto/create-teacher.dto.ts
+++ src/teachers/dto/create-teacher.dto.ts
@@ -1,1 +1,8 @@
-export class CreateTeacherDto {}
+import { Teacher } from '../entities/teacher.entity';
+
+export class CreateTeacherDto extends Teacher {
+  name: string;
+  email: string;
+  cpf: number | bigint;
+  formation: string;
+}
--- src/teachers/entities/teacher.entity.ts
+++ src/teachers/entities/teacher.entity.ts
@@ -1,1 +1,9 @@
-export class Teacher {}
+import { Prisma } from '@prisma/client';
+
+export class Teacher implements Prisma.TeacherUncheckedCreateInput {
+  id?: string;
+  name: string;
+  email: string;
+  cpf: number | bigint;
+  formation: string;
+}
